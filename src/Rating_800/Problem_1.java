71889db874a793c263ebd815606b14e9f5f0987c
Monu Kumar Singh <monusinghbharadwaj@gmail.com>
2024-03-23 19:45:01 +0530

    Problem_1

--- src/Rating_800/Problem_1.java
+++ src/Rating_800/Problem_1.java
@@ -1,2 +1,35 @@
-package Rating_800;public class Problem_1 {
+package Rating_800;
+import java.util.*;
+
+public class Problem_1 {
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int  n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] boxes = new int[n];
+            for (int i = 0; i<n;i++){
+                boxes[i] =sc.nextInt();
+            }
+
+            if(k==1 && !isSorted(boxes)){
+                System.out.println("NO");
+            }else {
+                System.out.println("YES");
+            }
+
+        }
+    }
 }
