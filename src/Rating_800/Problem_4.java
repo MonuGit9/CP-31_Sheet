02b960d65397945278784cfef97670def666c797
Monu Kumar Singh <121883072+MonuGit9@users.noreply.github.com>
2024-06-14 16:26:27 +0530

    Update Problem_4.java

--- src/Rating_800/Problem_4.java
+++ src/Rating_800/Problem_4.java
@@ -1,1 +1,19 @@
+package Rating_800;
 
+import java.util.*;
+
+public class Problem_4 {
+               public static void main(String[] args){
+                    Scanner sc = new Scanner(System.in);
+                    
+                    int t = sc.nextInt();
+                    
+                    while(t-->0){
+                                   int n = sc.nextInt();
+                                   
+                                   if(n%3==0)   System.out.println("Second");
+                                   else         System.out.println("First");
+                                                 
+                    }
+               }
+}
